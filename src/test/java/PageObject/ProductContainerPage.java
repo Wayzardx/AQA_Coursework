0a6812b2abdbcafac5be34c98acbbd761105840d
WAYZARD <wayzardz@gmail.com>
2023-05-10 04:09:39 +0300

    Update for my coursework. 1.Added waits 2.Increased stability tests

--- src/test/java/PageObject/ProductContainerPage.java
+++ src/test/java/PageObject/ProductContainerPage.java
@@ -1,19 +1,23 @@
 package PageObject;
+
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProductContainerPage {
 
-    private final SelenideElement addToCartButton = $(By.xpath("//*[@id=\"product\"]/div[1]/div/div/section[2]/div/div/div[4]/button[2]"));
-    private final SelenideElement BuyNowButton = $(By.xpath("//*[@id=\"product\"]/div[1]/div/div/section[2]/div/div/div[4]/button[1]"));
+    private final SelenideElement addToCartButton = $(By.xpath("//div[@class=\"product-sidebar__ordering\"]//button[@class=\"ui-btn-secondary add-to-cart\"]"));
+    private final SelenideElement BuyNowButton = $(By.xpath("//div[@class=\"product-sidebar__ordering\"]//button[@class=\"ui-btn-primary add-to-cart\"]"));
 
     public void clickAddToCartBtn() {
-        addToCartButton.click();
+        addToCartButton.shouldBe(Condition.exist, Duration.ofSeconds(5000)).click();
     }
 
     public void clickBuyNowBtn() {
-        BuyNowButton.click();
+        BuyNowButton.shouldBe(Condition.exist, Duration.ofSeconds(5000)).click();
     }
 }
